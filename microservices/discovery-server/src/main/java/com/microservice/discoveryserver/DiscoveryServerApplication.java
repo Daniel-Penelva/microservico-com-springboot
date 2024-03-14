7e8ee2602edfd989cf4e058251f47bc7f08199e2
Daniel Penelva <d4n.andrade@gmail.com>
2024-03-13 21:36:26 -0300

    microservice discovery-server (eureka) - criando arquivo de configuração (application.properties) discovery-server + add a anotação @EnableEurekaServer

--- microservices/discovery-server/src/main/java/com/microservice/discoveryserver/DiscoveryServerApplication.java
+++ microservices/discovery-server/src/main/java/com/microservice/discoveryserver/DiscoveryServerApplication.java
@@ -2,10 +2,13 @@ package com.microservice.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServerApplication {
 
+	// http://localhost:8761/
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServerApplication.class, args);
 	}
